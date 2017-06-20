98ad11647cd1db0f36466e2a6c43f3ea9c00d51f
Arunkumar Palaniappan <akrp@live.in>
2017-06-20 09:00:33 +0530

    Fixed DeprecationWarning: Archiver.bulk() deprecated since 0.21.0

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var fs = require('fs');
-var archiver = require('archiver');
+const fs = require('fs');
+const archiver = require('archiver');
 
-function zipFolder(srcFolder, zipFilePath, callback) {
+const zipFolder = (srcFolder, zipFilePath, callback) => {
 	var output = fs.createWriteStream(zipFilePath);
 	var zipArchive = archiver('zip');
-
+    
 	output.on('close', function() {
 		callback();
 	});
 
 	zipArchive.pipe(output);
 
-	zipArchive.bulk([
-		{ cwd: srcFolder, src: ['**/*'], expand: true }
-	]);
+	zipArchive.glob('**/*', 
+        { cwd: srcFolder, 
+          src: ['**/*'], 
+          expand: true }
+	);
 
 	zipArchive.finalize(function(err, bytes) {
 		if(err) {
